870c911cfb8d06b3095bb9dc1949841c9d34a135
ThariqXIRPL29 <87298575+thariqxrpl30@users.noreply.github.com>
2021-11-14 20:26:49 +0700

    Add files via upload

---
+++ restoran/admin/index.php
@@ -0,0 +1,80 @@
+<?php
+
+    session_start();
+    require_once "../dbcontroller.php";
+    $db = new DB;
+
+    if (!isset($_SESSION['user'])) {
+        header("location:login.php");
+    }
+    if (isset($_GET['log'])) {
+        session_destroy();
+        header("location:index.php");
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device=width,initial-scale-1.o">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <title>Admin Page | Aplikasi Restoran SMK</title>
+        <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    </head>
+
+    <body>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h2>Restoran</h2>
+                </div>
+
+                <div class="col-md-9">
+                    <div class="float-end mt-4"><a href="?log=logout">logout</a></div>
+                    <div class="float-end me-4 mt-4">user : <a href="?f=user&m=updateuser&id=<?php echo $_SESSION['iduser'] ?>"> <?php echo $_SESSION['user'] ?> </a> </div>
+                    
+                </div>
+            </div>
+
+
+            <div class="row mt-5">
+                <div class="col-md-3">
+                    
+                    <ul class="nav flex-column">
+                        <li class="nav-item"><a class="nav-link" href="?f=kategori&m=select">Kategori</a></li>
+                        <li class="nav-item"><a class="nav-link" href="?f=menu&m=select">Menu</a></li>
+                        <li class="nav-item"><a class="nav-link" href="?f=pelanggan&m=select">Pelanggan</a></li>
+                        <li class="nav-item"><a class="nav-link" href="?f=order&m=select">Order</a></li>
+                        <li class="nav-item"><a class="nav-link" href="?f=orderdetail&m=select">Order Detail</a></li>
+                        <li class="nav-item"><a class="nav-link" href="?f=user&m=select">User</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-9">
+                    <?php
+                        if (isset($_GET['f']) && isset($_GET['m'])) {
+                           $f = $_GET['f'];
+                           $m = $_GET['m'];
+
+                           $file = '../' . $f . '/' . $m . '.php';
+
+                           require_once $file;
+                        }
+
+                    ?>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                    <div class="col">
+                        <p class="text-center">2021 copyright@fancesatria.com</p>
+                    </div>
+            </div>
+        </div>
+
+    </body>
+</html>
